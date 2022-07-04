83d7059672224ebbac7d37f6db18bb39893dcb12
Tom Cant <tomcant@gmail.com>
2022-07-04 22:20:39 +0100

    Test the happy path (i.e. a child command that exits zero)

--- test.sh
+++ test.sh
@@ -4,6 +4,12 @@ BIN_PATH=$1
 [ ! -x "${BIN_PATH}" ] && { echo "error: required argument BIN_PATH is either missing or not executable" >&2; exit 1; }
 command -v jq >/dev/null || { echo "error: required dependency 'jq' is missing." >&2; exit 1; }
 
+givenScript() {
+  local script="$1"
+  echo -e "#!/bin/sh\n${script}" > ./script
+  chmod +x ./script
+}
+
 assertEqual() {
   local case="$1"
   local expected="$2"
@@ -39,6 +45,10 @@ assertEqual \
   "$(cargo metadata 2>/dev/null | jq -r '.packages[] | select(.name == "retry-cli") | .version')" \
   "$(${BIN_PATH} --version | cut -d' ' -f2)"
 
-echo -e "#!/bin/bash\nexit 2" > ./exit.sh && chmod +x ./exit.sh
-assertExitCode "It reflects the exit code of the child command" 2 "${BIN_PATH} -a 1 -- ./exit.sh"
-rm ./exit.sh
+givenScript "exit 0"
+assertExitCode "It exits successfully when the child command exits successfully" 0 "${BIN_PATH} -- ./script"
+
+givenScript "exit 2"
+assertExitCode "It reflects the non-zero exit code of the child command when it fails" 2 "${BIN_PATH} -a 1 -- ./script"
+
+rm ./script
